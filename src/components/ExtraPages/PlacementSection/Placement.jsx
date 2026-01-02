93ff3b9312fa9b3364ef957f6e543ef557901926
Prasanthso <prasanthsubramaniyan945@gmail.com>
2026-01-02 16:41:29 +0530

    placement

--- src/components/ExtraPages/PlacementSection/Placement.jsx
+++ src/components/ExtraPages/PlacementSection/Placement.jsx
@@ -227,7 +227,7 @@ const Placement = () => {
                 <img src={card1} />
               </div>
 
-              <div class="stat-value">151</div>
+              <div class="stat-value">180</div>
             </div>
 
             <div class="ps-stat-card blue center">
@@ -239,7 +239,7 @@ const Placement = () => {
                 <img src={card2} />
               </div>
 
-              <div class="stat-value big">794</div>
+              <div class="stat-value big">894</div>
             </div>
 
             <div class="ps-stat-card green">
@@ -251,7 +251,7 @@ const Placement = () => {
                 <img src={card3} />
               </div>
 
-              <div class="stat-value">35</div>
+              <div class="stat-value">35 LPA</div>
             </div>
           </div>
         </div>
