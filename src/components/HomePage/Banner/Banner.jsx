664bc51a3a9c33937df1c27f99bb61629a7d8279
Prasanthso <prasanthsubramaniyan945@gmail.com>
2026-02-06 10:31:37 +0530

    baner

--- src/components/HomePage/Banner/Banner.jsx
+++ src/components/HomePage/Banner/Banner.jsx
@@ -8,7 +8,7 @@ import "swiper/css/navigation";
 import "./Banner.css";
 
 import img1 from "../../../assets/images/1.png";
-import img2 from "../../../assets/images/2.png";
+// import img2 from "../../../assets/images/2.png";
 import img3 from "../../../assets/images/3.png";
 import img4 from "../../../assets/images/4.png";
 import img5 from "../../../assets/images/5.png";
@@ -22,7 +22,7 @@ import img12 from "../../../assets/images/12.png";
 import img13 from "../../../assets/images/13.png";
 
 export default function Banner() {
-  const images = [img1, img2, img10, img13, img11, img12,img3, img4, img5, img6, img7, img8, img9];
+  const images = [img1, img13, img11, img12,img3, img4, img5, img6, img7, img8, img9, img10];
 
   return (
     <div className="Banner-container">
