e67e93a7cf78db9afd1ac3bee76ef8c403b64d8e
webmaster <webmaster@kongu.edu>
2026-02-16 12:20:02 +0530

    testimonials updated

--- src/components/HomePage/Testimonials/Testimonials.jsx
+++ src/components/HomePage/Testimonials/Testimonials.jsx
@@ -10,6 +10,12 @@ import vignesh from "../../../assets/images/Testimonials/vignesh.jpeg";
 import dhanraj from "../../../assets/images/Testimonials/dhanraj.jpeg";
 import aravindh from "../../../assets/images/Testimonials/aravindh.jpeg";
 import siva from "../../../assets/images/Testimonials/siva.jpeg";
+import sahithya from "../../../assets/images/Testimonials/sahithya.jpeg";
+import alex from "../../../assets/images/Testimonials/alex.jpg";
+import muruganandam from "../../../assets/images/Testimonials/muruganandam.jpeg";
+import suhaag from "../../../assets/images/Testimonials/suhaag.jpeg";
+import daniel from "../../../assets/images/Testimonials/daniel.jpeg";
+import menaka from "../../../assets/images/Testimonials/menaka.jpeg";
 
 const testimonialsData = [
     {
@@ -118,7 +124,7 @@ const testimonialsData = [
     },
      {
         id: 14,
-        name: "S.Aravindh",
+        name: "S. Aravindh",
         role: "Associate Full Stack Engineer, Rubrik, Bangalore",
         avatar: aravindh,
         content:
@@ -131,6 +137,54 @@ const testimonialsData = [
         avatar: siva,
         content:
             "I am a proud 2015 graduate of the KEC, IT Department, and looking back, I am filled with gratitude for the incredible foundation it gave me. The department offered outstanding facilities, a supportive learning environment, and faculty members who treated every student like family. The friendships, the culture, and the overall atmosphere shaped not just our academic journey but who we became as individuals. The department has always been known for producing students with strong technical caliber and grounded values, and I’m happy to see that tradition continuing even today. A special thanks to SVG sir, whose personal connection with students and alumni remains unmatched. His guidance, warmth, and continued engagement with us long after graduation speak volumes about the heart of the department. It’s inspiring to see the IT Department still nurturing exceptional talent and helping students shine in their careers. I am truly grateful to have been a part of it.",
+    },
+      {
+        id: 16,
+        name: "Sahithya",
+        role: "Software Engineer, HCL GUVI",
+        avatar: sahithya,
+        content:
+            "I am Sahithya from the 2020-2024 B.E. Computer Science and Engineering batch of Kongu Engineering College. My journey at CSE-KEC was shaped by strong technical learning, hands-on projects, and active leadership across academic, service, and cultural platforms, which strengthened my problem-solving, communication, and decision-making skills. These experiences built a solid foundation for my growth as a Software Engineer. I am grateful to my professors, advisors, and HODs for their continuous guidance, and being recognized as the Best Outgoing Student of the CSE Department (2020-2024) stands as a proud reflection of the empowering ecosystem at KEC.",
+    },
+       {
+        id: 17,
+        name: "S. Alex Raj",
+        role: "Lead Developer Engineer, COMCAST",
+        avatar: alex,
+        content:
+            "My journey at Kongu Engineering College within the Computer Science and Engineering department was a period of immense growth. The department's comprehensive syllabus provided me with a deep, fundamental knowledge of the CSE domain, which has been instrumental in my career progression. Additionally, the structured placement preparation was a key factor in building my confidence and technical expertise, enabling me to successfully crack multiple interviews. Currently, I am specializing in the Cyber Security domain at Comcast India Engineering Centre, and I owe much of my professional success to the strong foundation laid at KEC.",
+    },
+       {
+        id: 18,
+        name: "V. Muruganandam",
+        role: "General Manager, Airports Authority of India, Chennai",
+        avatar: muruganandam,
+        content:
+            "Kongu Engineering College is my esteemed institution, for the invaluable pedagogical foundation that shaped my career. Today as a valued aviation professional, I remain deeply grateful to KEC for the strong engineering base it provided. I look forward to giving back to my alma mater and contributing to its growth in the future.",
+    },
+    {
+        id: 19,
+        name: "Suhaag Kumar",
+        role: "CEO Mokshaa Group of Companies, Seattle, WA USA",
+        avatar: suhaag,
+        content:
+            "Kongu Engineering College is a foundation for excellence, leadership, and lifelong success. My years at KEC, especially in the Department of Electronics and Communication Engineering, shaped my thinking, discipline, and global outlook. The culture of hard work, integrity, and continuous learning at Kongu continues to influence every decision I make as a leader. I carry the spirit of Kongu with pride wherever I go. My journey from KEC to global organizations and entrepreneurship stands as a testament to the strong foundation this institution provides to its students.",
+    },
+     {
+        id: 20,
+        name: "Daniel Jacob",
+        role: "Senior Vice President, EC-Group DataSoft Pvt Ltd, Chennai",
+        avatar: daniel,
+        content:
+            "My time at Kongu Engineering College in the Computer Technology (UG) Department was truly transformative. Coming from a government school, vernacular background, I initially struggled with fear and self-doubt. The constant encouragement and belief in me from my professors gave me the confidence to succeed. The excellent infrastructure and exposure through events and activities played a crucial role in shaping my learning journey and the person I am today. I remain deeply grateful to Kongu.",
+    },
+     {
+        id: 21,
+        name: "Menaka",
+        role: "Cloud and DevOps Engineer, Presidio",
+        avatar: menaka,
+        content:
+            "The supportive faculty and well-equipped labs gave me the flexibility to explore, learn and adapt to new technologies. Collaborative projects helped me grow as a team player and develop leadership skills. Being actively involved in department activities and receiving continuous guidance for placements played a key role in shaping my career. The exposure to real-time problem solving boosted my confidence to take on challenges. I am grateful for the strong foundation that continues to drive my professional growth.",
     },
 ];
 
