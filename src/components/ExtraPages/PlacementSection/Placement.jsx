2caec77fd245dfde261d4d791a6f94325edc4e67
webmaster <webmaster@kongu.edu>
2026-03-13 12:33:57 +0530

    placement updated

--- src/components/ExtraPages/PlacementSection/Placement.jsx
+++ src/components/ExtraPages/PlacementSection/Placement.jsx
@@ -238,7 +238,7 @@ const Placement = () => {
                 <img src={card1} />
               </div>
 
-              <div class="stat-value">278</div>
+              <div class="stat-value">279</div>
             </div>
 
             <div class="ps-stat-card blue center">
@@ -250,7 +250,7 @@ const Placement = () => {
                 <img src={card2} />
               </div>
 
-              <div class="stat-value big">1223</div>
+              <div class="stat-value big">1224</div>
             </div>
 
             <div class="ps-stat-card green">
