1c4b68ce7fe79d5917e0c043d384a927ba0e0801
webmaster <webmaster@kongu.edu>
2026-03-24 09:47:41 +0530

    navbar updated

--- src/components/HomePage/navbar/Navbar.jsx
+++ src/components/HomePage/navbar/Navbar.jsx
@@ -203,6 +203,14 @@ const Navbar = () => {
                 >
                   ⚡ International conference - NG-CAST 2026
                 </a>
+
+                 <a
+                  href="https://grad2026.kongu.edu/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  ⚡ 38th Graduation Day
+                </a>
               </div>
 
             </div>
