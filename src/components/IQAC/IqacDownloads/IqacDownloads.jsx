6a02558591e7fd14b29bb37a2827dae8d1a4924f
webmaster <webmaster@kongu.edu>
2026-04-11 12:06:50 +0530

    iqac updated

--- src/components/IQAC/IqacDownloads/IqacDownloads.jsx
+++ src/components/IQAC/IqacDownloads/IqacDownloads.jsx
@@ -25,6 +25,7 @@ const copoFiles = [
     { name: "5.Project_Work sixth sem V2.xlsx", path: `${BASE_PATH}/copo/5.Project_Work sixth sem V2.xlsx` }
 ];
 
+
 const pdfFiles = [
     "1_Timetable v2.pdf", "2_Academic V2.pdf", "3_Project V1.pdf", "4_CO-PO v1.pdf",
     "5_Lab.pdf", "6_Feedback v2.pdf", "7_IIPC.pdf", "8_Research.pdf",
@@ -61,11 +62,11 @@ const eomsCopoFiles = [
 
 const excel = [
     { name: "1.AC-06.xlsx", path: `${EOMS_BASE_PATH}/MDF/WORD/1.AC-06.xlsx` },
-    { name: "2.AC-07.xlsx", path: `${EOMS_BASE_PATH}/COPO/2.AC-07.xlsx` },
-    { name: "3.AC-08.xlsx", path: `${EOMS_BASE_PATH}/COPO/3.AC-08.xlsx` },
-    { name: "4.CDC_05.xlsx", path: `${EOMS_BASE_PATH}/COPO/4.CDC_05.xlsx` },
-    { name: "5.CDC_07.xlsx", path: `${EOMS_BASE_PATH}/COPO/5.CDC_07.xlsx` },
-    { name: "6.CDC_08.xlsx", path: `${EOMS_BASE_PATH}/COPO/6.CDC_08.xlsx` }
+    { name: "2.AC-07.xlsx", path: `${EOMS_BASE_PATH}/MDF/WORD/2.AC-07.xlsx` },
+    { name: "3.AC-08.xlsx", path: `${EOMS_BASE_PATH}/MDF/WORD/3.AC-08.xlsx` },
+    { name: "4.CDC_05.xlsx", path: `${EOMS_BASE_PATH}/MDF/WORD/4.CDC_05.xlsx` },
+    { name: "5.CDC_07.xlsx", path: `${EOMS_BASE_PATH}/MDF/WORD/5.CDC_07.xlsx` },
+    { name: "6.CDC_08.xlsx", path: `${EOMS_BASE_PATH}/MDF/WORD/6.CDC_08.xlsx` }
 ];
 
 const eomsMdfFiles = [
